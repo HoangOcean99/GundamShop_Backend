1da35db3f4a8f481de3411508487a8e57b2f8be5
TheDarkDuck <ngoducanhzza@gmail.com>
2026-03-15 02:18:00 +0700

    format

--- gundam-shop-backend/models/Category.js
+++ gundam-shop-backend/models/Category.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 
-const CategorySchema = new mongoose.Schema(
-  {
+const CategorySchema = new mongoose.Schema({
     name: { type: String, required: true },
-    description: String,
-  },
-  { timestamps: true },
-);
+  description: String
+}, { timestamps: true });
 
 module.exports = mongoose.model("Category", CategorySchema);
